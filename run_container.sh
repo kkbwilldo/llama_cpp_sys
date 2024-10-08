a5be605c85f3d6d2558779c7f5013fe1a5aba1d9
kkbwilldo <kimkyu1515@naver.com>
2024-10-08 13:50:29 +0900

    [feat]: add run script for the container

---
+++ run_container.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+docker run \
+  --gpus all \
+  -it \
+  --rm \
+  --name llama_cpp \
+  --cap-add=SYS_ADMIN \
+  -v ./checkpoints_42dot_LLM-PLM-1.3B:/home/kbkim/checkpoints_42dot_LLM-PLM-1.3B:ro \
+  -v ./run_inference.sh:/home/kbkim/run_inference.sh \
+  -v ./base_inference.py:/home/kbkim/base_inference.py \
+  -v ./outputs:/home/kbkim/outputs \
+  llama_cpp:pytorch-24.02
+
+# --cap-add=SYS_ADMIN: for ncu
+# --gpus all: use all gpus
+# --gpus '"device=0"': use specific gpu
