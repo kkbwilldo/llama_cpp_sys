236e33a077a44c6bd255edacaee59aba7b9728a7
kkbwilldo <kimkyu1515@naver.com>
2024-10-08 22:56:01 +0900

    [feat]: mount run script for mmlu benchmark on the container run script

--- run_container.sh
+++ run_container.sh
@@ -8,6 +8,8 @@ docker run \
   --cap-add=SYS_ADMIN \
   -v ./checkpoints_42dot_LLM-PLM-1.3B:/home/kbkim/checkpoints_42dot_LLM-PLM-1.3B:ro \
   -v ./run_inference.sh:/home/kbkim/run_inference.sh \
+  -v ./run_download_mmlu_dataset.sh:/home/kbkim/run_download_mmlu_dataset.sh \
+  -v ./run_mmlu_bench.sh:/home/kbkim/run_mmlu_bench.sh \
   -v ./base_inference.py:/home/kbkim/base_inference.py \
   -v ./outputs:/home/kbkim/outputs \
   -e HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN \
