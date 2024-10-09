5d09e8a67318cba4f025073f99d1fc9c149b58bd
kkbwilldo <kimkyu1515@naver.com>
2024-10-09 20:56:47 +0900

    [fix]: use absolute path for all mountings

--- scripts_run/run_container.sh
+++ scripts_run/run_container.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# 현재 스크립트의 경로를 기준으로 프로젝트 루트 디렉토리 설정
+PROJECT_ROOT=$(dirname $(dirname $(realpath $0)))
+
 docker run \
   --gpus all \
   -it \
@@ -7,14 +10,14 @@ docker run \
   --network=host \
   --name llama_cpp \
   --cap-add=SYS_ADMIN \
-  -v ../checkpoints_42dot_LLM-PLM-1.3B:/home/kbkim/checkpoints_42dot_LLM-PLM-1.3B:ro \
-  -v ./run_download_mmlu_dataset.sh:/home/kbkim/run_download_mmlu_dataset.sh \
-  -v ./run_mmlu_bench.sh:/home/kbkim/run_mmlu_bench.sh \
-  -v ./run_llama_cpp_server.sh:/home/kbkim/run_llama_cpp_server.sh \
-  -v ./run_locust.sh:/home/kbkim/run_locust.sh \
-  -v ./run_checkpoint_to_gguf_conversion.sh:/home/kbkim/run_checkpoint_to_gguf_conversion.sh \
-  -v ../locustfile.py:/home/kbkim/locustfile.py \
-  -v ../outputs:/home/kbkim/outputs \
+  -v $PROJECT_ROOT/checkpoints_42dot_LLM-PLM-1.3B:/home/kbkim/checkpoints_42dot_LLM-PLM-1.3B:ro \
+  -v $PROJECT_ROOT/scripts/run_download_mmlu_dataset.sh:/home/kbkim/run_download_mmlu_dataset.sh \
+  -v $PROJECT_ROOT/scripts/run_mmlu_bench.sh:/home/kbkim/run_mmlu_bench.sh \
+  -v $PROJECT_ROOT/scripts/run_llama_cpp_server.sh:/home/kbkim/run_llama_cpp_server.sh \
+  -v $PROJECT_ROOT/scripts/run_locust.sh:/home/kbkim/run_locust.sh \
+  -v $PROJECT_ROOT/scripts/run_checkpoint_to_gguf_conversion.sh:/home/kbkim/run_checkpoint_to_gguf_conversion.sh \
+  -v $PROJECT_ROOT/locustfile.py:/home/kbkim/locustfile.py \
+  -v $PROJECT_ROOT/outputs:/home/kbkim/outputs \
   -e HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN \
   llama_cpp:pytorch-24.02
 
