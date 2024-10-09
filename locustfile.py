7fd25eba2c3306fb7ee9ff400b80e80c5d82714e
kkbwilldo <kimkyu1515@naver.com>
2024-10-09 18:24:33 +0900

    [feat]: add support for locust profiling of multiple user requests

---
+++ locustfile.py
@@ -0,0 +1,26 @@
+from locust import HttpUser, TaskSet, task
+
+class LlamaCPPTest(TaskSet):
+    """
+    llama.cpp 서버에 요청을 보내는 테스트 클래스
+    """
+    @task
+    def llama_request(self):
+        """
+        llama.cpp 서버에 요청을 보내는 함수
+        """
+        # llama-server로 보낼 요청 데이터 예시
+        payload = {
+            "prompt": "Building a website can be done in 10 simple steps:",
+            "n_predict": 768
+        }
+
+        # llama-server에 /completion 경로로 POST 요청 보내기
+        self.client.post("/completion", json=payload)
+
+class LlamaUser(HttpUser):
+    """
+    llama.cpp 서버에 요청을 보내는 사용자 클래스
+    """
+    tasks = [LlamaTest]
+    host = "http://localhost:8080"  # llama-server 주소
