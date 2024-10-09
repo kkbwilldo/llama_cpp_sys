f6f69ce8fe932f37eccd9d40cc8c6c73c04716f8
kkbwilldo <kimkyu1515@naver.com>
2024-10-09 18:27:42 +0900

    [feat]: add support for llama server and locust on run script of the container

--- run_container.sh
+++ run_container.sh
@@ -4,13 +4,15 @@ docker run \
   --gpus all \
   -it \
   --rm \
+  --network=host \
   --name llama_cpp \
   --cap-add=SYS_ADMIN \
   -v ./checkpoints_42dot_LLM-PLM-1.3B:/home/kbkim/checkpoints_42dot_LLM-PLM-1.3B:ro \
-  -v ./run_inference.sh:/home/kbkim/run_inference.sh \
   -v ./run_download_mmlu_dataset.sh:/home/kbkim/run_download_mmlu_dataset.sh \
   -v ./run_mmlu_bench.sh:/home/kbkim/run_mmlu_bench.sh \
-  -v ./base_inference.py:/home/kbkim/base_inference.py \
+  -v ./run_llama_cpp_server.sh:/home/kbkim/run_llama_cpp_server.sh \
+  -v ./run_locust.sh:/home/kbkim/run_locust.sh \
+  -v ./locustfile.py:/home/kbkim/locustfile.py \
   -v ./outputs:/home/kbkim/outputs \
   -e HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN \
   llama_cpp:pytorch-24.02
