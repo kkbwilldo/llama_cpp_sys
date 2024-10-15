37ea18de4fdc60d0f009086ca00a55c2fdcb6885
kkbwilldo <kimkyu1515@naver.com>
2024-10-15 22:42:39 +0900

    [feat]: modify mmlu bench script to adjust context size via input argument

--- scripts_run/run_mmlu_bench.sh
+++ scripts_run/run_mmlu_bench.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-MODEL_PATH="./outputs/checkpoints_42dot_LLM-PLM-1.3B-42dot-PLM-F16.gguf"
+# MODEL_PATH="./outputs/checkpoints_42dot_LLM-PLM-1.3B-42dot-PLM-F16.gguf"
+MODEL_PATH="./outputs/ggml-model-Q4_0.gguf"
 DATA_PATH="./mmlu.bin"
-CONTEXT_SIZE=1024
+CONTEXT_SIZE=2048
 
 ./llama.cpp/llama-perplexity \
   --multiple-choice \
   -m $MODEL_PATH \
   -bf $DATA_PATH \
-  --ctx-size $CONTEXT_SIZE
+  --ctx-size $CONTEXT_SIZE \
+  |& tee ./outputs/mmlu_f16_pre_tok_gpt2.log
