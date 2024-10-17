51f1f469e2745461431a3b155c579fa1489b8485
kkbwilldo <kimkyu1515@naver.com>
2024-10-17 20:07:58 +0900

    [fix]: fix issue with argument recognition by changing $VAR to "$VAR" in variable usage

--- scripts_run/run_model_quantization.sh
+++ scripts_run/run_model_quantization.sh
@@ -4,4 +4,4 @@ MODEL_DIR="./outputs/base_fp16.gguf"
 QUANT_TYPE="Q4_K_S"
 IMATRIX_DIR="./outputs/imatrix_groups_merged_enhanced_v3.dat"
 
-./llama.cpp/llama-quantize --imatrix $IMATRIX  $MODEL_DIR $QUANT_TYPE
+./llama.cpp/llama-quantize --imatrix "$IMATRIX"  "$MODEL_DIR" "$QUANT_TYPE"
