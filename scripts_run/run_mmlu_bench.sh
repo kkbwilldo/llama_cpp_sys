1c93d6c78982438c2b56acb761f11bd5fd839945
kkbwilldo <kimkyu1515@naver.com>
2024-10-19 10:46:57 +0900

    [refactor]: modify hardcoded script to accept arguments

--- scripts_run/run_mmlu_bench.sh
+++ scripts_run/run_mmlu_bench.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# MODEL_PATH="./outputs/checkpoints_42dot_LLM-PLM-1.3B-42dot-PLM-F16.gguf"
-MODEL_PATH="./outputs/ggml-model-Q4_0.gguf"
+if [ -z "$1" ]; then
+  MODEL_PATH="./outputs/base_fp16.gguf"
+else
+  MODEL_PATH="./outputs/$1"
+fi
+
+
+if [ -z "$2" ]; then
+  BUILD_NUM="1"
+else
+  BUILD_NUM="$2"
+fi
+
+
 DATA_PATH="./mmlu.bin"
 CONTEXT_SIZE=2048
+
+# MODEL_PATH에서 파일명만 추출
+MODEL_FILENAME=$(basename "$MODEL_PATH")
 
 ./llama.cpp/llama-perplexity \
   --multiple-choice \
   -m $MODEL_PATH \
   -bf $DATA_PATH \
   --ctx-size $CONTEXT_SIZE \
-  |& tee ./outputs/mmlu_f16_pre_tok_gpt2.log
+  |& tee "./outputs/build_test/build_${BUILD_NUM}_inference_test_${MODEL_FILENAME}.log"
