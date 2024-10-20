da37ec9daf7b982682fc489af6e53d710327fab0
kkbwilldo <kimkyu1515@naver.com>
2024-10-20 11:09:02 +0900

    [feat]: add flags when run the server

--- scripts_run/run_llama_cpp_server.sh
+++ scripts_run/run_llama_cpp_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-MODEL_DIR=./outputs/checkpoints_42dot_LLM-PLM-1.3B-42dot-PLM-F16.gguf
-CONTEXT_SIZE=2560
+MODEL_DIR=./outputs/base_fp16_fa_build.gguf
+# MODEL_DIR=./outputs/ggml-model-Q4_K_S_fa_build.gguf
+CONTEXT_SIZE=26880 # (2560+768+32)*8
+N_GPU_LAYERS=30
+N_PARALLEL=8
 
-./llama.cpp/llama-server -m $MODEL_DIR --ctx-size $CONTEXT_SIZE
+./llama.cpp/llama-server -fa -ngl $N_GPU_LAYERS -np $N_PARALLEL -m $MODEL_DIR --ctx-size $CONTEXT_SIZE
+# sudo nsys profile --output llama_profile_3 ./llama.cpp/llama-server -fa -ngl $N_GPU_LAYERS -np $N_PARALLEL -m $MODEL_DIR --ctx-size $CONTEXT_SIZE
